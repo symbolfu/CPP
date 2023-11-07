3236b7133cf1dbddd48bca074a0d09a967031516
symbolfu <fh_swjt@163.com>
2023-11-07 21:36:11 +0800

    add namespace and scope

---
+++ part_07/day02_00/day02_00/day02_00.cpp
@@ -0,0 +1,32 @@
+
+/*
+	内存模型和名称空间
+		这部分主要是一些概念性的东西：
+			编译
+			存储持续性
+			作用域
+			链接性 （单元间的可见性）
+			new的另类用法：定位运算符new
+				int a = new(datatype1) datatype2 
+				即从datatype1可用空间中分配处datatype2的空间；
+			名称空间：
+				namespace
+					
+				导入名称空间内的符号：
+					1、using声明
+						using name::var
+					2、using编译指令
+						using namespace name
+						
+				名称空间别名：
+					namespace newname = name
+					
+
+*/
+
+int main()
+{
+
+
+	return 0;
+}
